566d4f3e5d3b0fc1507a803adac647b4790fa382
B6O6S <114145818+B6O6S@users.noreply.github.com>
2025-09-07 08:52:46 +0300

    Update Better ODH Auto Updater.lua

--- Better ODH Auto Updater.lua
+++ Better ODH Auto Updater.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/B6O6S/Better-ODH/refs/heads/main/Auto%20Updater.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/B6O6S/Better-ODH/refs/heads/main/Better%20ODH%20loader.lua",true))()
