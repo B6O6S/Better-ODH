a53c44ef18e9cb5c59faf1fb40256c24c360a155
B6O6S <114145818+B6O6S@users.noreply.github.com>
2026-09-11 16:43:31 +0300

    Update Main loader.lua

--- Main loader.lua
+++ Main loader.lua
@@ -17,4 +17,5 @@ shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/CODM%20Guns"
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Juke%20sound%20effect")
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/RTX%20%26%20Graphics")
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Death%20Sound%20Effect")
+shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Jump%20Sound%20Effect")
 shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Credits")
