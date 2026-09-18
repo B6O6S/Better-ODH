9801739b35502a1679d828e72f6cb26ea2a1c5c2
B6O6S <114145818+B6O6S@users.noreply.github.com>
2026-09-18 16:44:45 +0300

    Update Main loader.lua

--- Main loader.lua
+++ Main loader.lua
@@ -19,6 +19,7 @@ task.spawn(function()
     shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/RTX%20%26%20Graphics")
     shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Death%20Sound%20Effect")
     shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Jump%20Sound%20Effect")
+    shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/SprayPaint%20Mods")
     shared.load_from_github_url("/B6O6S/Better-Odh-tabs/refs/heads/main/Credits")
 end)
 
