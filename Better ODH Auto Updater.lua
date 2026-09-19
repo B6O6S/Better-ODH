9af23694747a4d7880eee63ceaebb71b1901a3d2
B6O6S <114145818+B6O6S@users.noreply.github.com>
2026-09-19 14:53:49 +0300

    Update Better ODH Auto Updater.lua

--- Better ODH Auto Updater.lua
+++ Better ODH Auto Updater.lua
@@ -1,2 +1,2 @@
 local shared = odh_shared_plugins
-shared.load_from_github_url("/B6O6S/Better-ODH/refs/heads/main/Main loader.lua")
+shared.load_from_github_url("/B6O6S/Better-ODH/refs/heads/main/Main Loader.lua")
